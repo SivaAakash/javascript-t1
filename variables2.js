3db9764a49a1e6fa119cfd68a05c6ca19a62f95a
SivaAakash <sivaaakashtd@gmail.com>
2026-02-02 22:47:26 +0530

    Add files via upload

---
+++ variables2.js
@@ -0,0 +1,18 @@
+// Step 1: Create a CONST variable and assign a value
+  const a= 10;
+
+  // Step 2: Create a function
+  function values() {
+
+  
+    // Print the value
+    console.log("Value inside function:", a);
+  }
+
+  // Step 4: Call the function
+  values();
+
+  // Step 5: Try to assign another value to the CONST variable
+ 
+  // Step 6: Print the value
+  console.log("Value outside function:", a);
