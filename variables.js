c116ced6d98b3fedf520d7c852a1f0a285a590b0
SivaAakash <sivaaakashtd@gmail.com>
2026-02-02 22:41:35 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,19 @@
+// Step 1: Create a CONST variable and assign a value
+  const a = 10;
+
+  // Step 2: Create a function
+  function myFunction() {
+    // Step 3: Declare a variable using let and assign a value
+    let b= 5;
+
+    // Print the let variable
+    console.log("Value of let variable:", b);
+  }
+
+  // Step 4: Call the function
+  myFunction();
+
+  // Step 5: Try to assign a new value to the CONST variable
+
+  // Step 6: Print the CONST variable
+  console.log("Value of const variable:", a);
